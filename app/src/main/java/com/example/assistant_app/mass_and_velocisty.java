c79066fb4bbd056c9c4f5be697b7b54042993281
KarenGhardashyan <karenkrakin@gmail.com>
2024-01-01 17:37:58 +0400

    Initial commit

---
+++ app/src/main/java/com/example/assistant_app/mass_and_velocisty.java
@@ -0,0 +1,14 @@
+package com.example.assistant_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class mass_and_velocisty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mass_and_velocisty);
+    }
+}
---
+++ app/src/main/java/com/example/assistant_app/physics_lobby.java
@@ -0,0 +1,14 @@
+package com.example.assistant_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class physics_lobby extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_physics_lobby);
+    }
+}
